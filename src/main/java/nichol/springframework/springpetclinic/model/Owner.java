edade10d77ac5a0176286fc48325cccddde314d7
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-14 16:39:24 +0700

    Adding POJOs. Closes task #2

---
+++ src/main/java/nichol/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package nichol.springframework.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/nichol/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package nichol.springframework.springpetclinic.model;
+
+public class Vet extends Person {
+
+}
