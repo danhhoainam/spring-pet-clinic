ce9c71b09d1603cd24ff2208286df0bc435a04e3
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-15 16:19:20 +0700

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package nichol.springframework.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
 	private String firstName;
 	private String lastName;
--- pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package nichol.springframework.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
 	private String name;
 
