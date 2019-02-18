efc61a197ad2cf317db901ae195d87592cf38140
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-18 13:45:51 +0700

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/services/VetService.java
@@ -3,13 +3,6 @@ package nichol.springframework.springpetclinic.services;
 
 import nichol.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-	Vet findById(Long id);
-
-	Vet save(Vet vet);
-
-	Set<Vet> findAll();
 }
