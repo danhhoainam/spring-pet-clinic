76c29497344f25f8604ae0238f1b20bc3dd0589f
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-21 16:36:59 +0700

    Added Specialties Map Impl

---
+++ pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package nichol.springframework.springpetclinic.services;
+
+import nichol.springframework.springpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
