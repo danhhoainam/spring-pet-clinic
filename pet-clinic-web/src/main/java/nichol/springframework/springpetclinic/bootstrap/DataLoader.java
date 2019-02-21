1636c5fcf6dcbb3d305024d3a412716d713bde76
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-21 16:25:03 +0700

    Added Pets and Contact info to owners

--- pet-clinic-web/src/main/java/nichol/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/nichol/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package nichol.springframework.springpetclinic.bootstrap;
 
 import nichol.springframework.springpetclinic.model.Owner;
+import nichol.springframework.springpetclinic.model.Pet;
 import nichol.springframework.springpetclinic.model.PetType;
 import nichol.springframework.springpetclinic.model.Vet;
 import nichol.springframework.springpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import nichol.springframework.springpetclinic.services.PetTypeService;
 import nichol.springframework.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,11 +39,33 @@ public class DataLoader implements CommandLineRunner {
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Micheal");
 		owner1.setLastName("Owen");
+		owner1.setAddress("123 Lang ha");
+		owner1.setCity("Hanoi");
+		owner1.setTelephone("222222222");
+
+		Pet pet1 = new Pet();
+		pet1.setPetType(savedDogType);
+		pet1.setOwner(owner1);
+		pet1.setBirthDate(LocalDate.now());
+		pet1.setName("Rusty");
+		owner1.getPets().add(pet1);
+
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Micheal");
 		owner2.setLastName("Bubble");
+		owner2.setAddress("555 Lang ha");
+		owner2.setCity("Hanoi");
+		owner2.setTelephone("444444");
+
+		Pet pet2 = new Pet();
+		pet2.setPetType(savedCatType);
+		pet2.setOwner(owner2);
+		pet2.setBirthDate(LocalDate.now());
+		pet2.setName("Mickey");
+		owner2.getPets().add(pet2);
+
 		ownerService.save(owner2);
 
 		System.out.println("Loaded Owners...");
