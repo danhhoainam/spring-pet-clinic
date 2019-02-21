657d37031d97671755c1b5866987727ee9d85432
Nguyen Danh Hoai Nam <hoainam.nguyen@ascendcorp.com>
2019-02-21 15:57:13 +0700

    added PetType

---
+++ pet-clinic-data/src/main/java/nichol/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package nichol.springframework.springpetclinic.services;
+
+import nichol.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
